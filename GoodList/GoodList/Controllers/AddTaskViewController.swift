749e8c0b3c9704c5f40c0296b3de3573617e5d0c
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-10 15:47:50 -0300

    Passing Task to Task List View Screen

--- GoodList/GoodList/Controllers/AddTaskViewController.swift
+++ GoodList/GoodList/Controllers/AddTaskViewController.swift
@@ -11,6 +11,11 @@ import RxSwift
 
 class AddTaskViewController: UIViewController {
     
+    private let taskSubject = PublishSubject<Task>()
+    var taskSubjectObservable: Observable<Task> {
+        return taskSubject.asObservable()
+    }
+    
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
     @IBOutlet weak var taskTitleTexField: UITextField!
     
@@ -22,5 +27,8 @@ class AddTaskViewController: UIViewController {
         }
 
         let task = Task(title: title, priority: priority)
+        taskSubject.onNext(task)
+        
+        self.dismiss(animated: true)
     }
 }
